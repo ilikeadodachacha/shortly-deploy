47224ae4bba0f877b55cc9433f4c0bb2d97a6bb9
Landon Roberts <landonmarkroberts@gmail.com>
2017-08-03 23:01:41 -0500

    Refactor schemas in link and user.js, refactor functions in request-handler.js

--- app/models/link.js
+++ app/models/link.js
@@ -2,9 +2,6 @@ var db = require('../config');
 var crypto = require('crypto');
 var mongoose = require('mongoose');
 
-
-var Schema = mongoose.Schema;
-
 var linkSchema = mongoose.Schema({
   'url': String,
   'base_url': String,
@@ -15,19 +12,11 @@ var linkSchema = mongoose.Schema({
 
 var Link = mongoose.model('Link', linkSchema);
 
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function() {
-//     this.on('creating', function(model, attrs, options) {
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
+linkSchema.pre('save', function(next) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+  next();
+});
 
 module.exports = Link;
