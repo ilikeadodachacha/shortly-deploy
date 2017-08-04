7ad993b3d5cd00778dc5d4f47a227d4b756a9ce1
Landon Roberts <landonmarkroberts@gmail.com>
2017-08-03 22:09:13 -0500

    Begin refactor of shortly-deploy

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,33 @@
 var db = require('../config');
 var crypto = require('crypto');
+var mongoose = require('mongoose');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+
+var Schema = mongoose.Schema;
+
+var linkSchema = mongoose.Schema({
+  'url': String,
+  'base_url': String,
+  'code': String,
+  'title': String,
+  'visits': Number
 });
+
+var Link = mongoose.model('Link', linkSchema);
+
+// var Link = db.Model.extend({
+//   tableName: 'urls',
+//   hasTimestamps: true,
+//   defaults: {
+//     visits: 0
+//   },
+//   initialize: function() {
+//     this.on('creating', function(model, attrs, options) {
+//       var shasum = crypto.createHash('sha1');
+//       shasum.update(model.get('url'));
+//       model.set('code', shasum.digest('hex').slice(0, 5));
+//     });
+//   }
+// });
 
 module.exports = Link;
